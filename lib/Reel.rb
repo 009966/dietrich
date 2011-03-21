8fb01d1d00e5aab24b56b6a70d0e09d833a372e8
Wolfgang Woehl <tito@online.de>
2011-03-21 12:42:30 +0100

    lib/Reel.rb: clear

--- lib/Reel.rb
+++ lib/Reel.rb
@@ -11,14 +11,14 @@ class Reel
   end
 end
 
-mainpicture = { :uuid => '1234-5678', :editrate => '24 1', :intrinsicduration => 5000, :entrypoint => 0, :duration => 5000, :keyid => nil, :hash => nil, :framerate => '24 1', :aspectratio => '1.85' }
-mainsound = { :uuid => '2345-6789', :editrate => '24 1', :intrinsicduration => 5000, :entrypoint => 0, :duration => 5000, :keyid => nil, :hash => nil }
-mainsubtitle = {  }
-
-r = Reel.new( 'uuid-1234-reel', 'Reel 1' )
-r.add_assets( mainpicture, mainsound, mainsubtitle )
-
-p r[ :annotationtext ]
-p r[ :assetlist ][ :mainpicture ]
-p r[ :assetlist ][ :mainsound ]
-p r[ :assetlist ][ :mainsubtitle ]
+#mainpicture = { :uuid => '1234-5678', :editrate => '24 1', :intrinsicduration => 5000, :entrypoint => 0, :duration => 5000, :keyid => nil, :hash => nil, :framerate => '24 1', :aspectratio => '1.85' }
+#mainsound = { :uuid => '2345-6789', :editrate => '24 1', :intrinsicduration => 5000, :entrypoint => 0, :duration => 5000, :keyid => nil, :hash => nil }
+#mainsubtitle = {  }
+#
+#r = Reel.new( 'uuid-1234-reel', 'Reel 1' )
+#r.add_assets( mainpicture, mainsound, mainsubtitle )
+#
+#p r[ :annotationtext ]
+#p r[ :assetlist ][ :mainpicture ]
+#p r[ :assetlist ][ :mainsound ]
+#p r[ :assetlist ][ :mainsubtitle ]
